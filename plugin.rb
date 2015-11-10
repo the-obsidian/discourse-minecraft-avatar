7252e999b67be358ade5c016365ebd4bf5d86476
Jacob Gillespie <jacobwgillespie@gmail.com>
2015-11-10 08:33:59 -0600

    Use after_initialize

--- plugin.rb
+++ plugin.rb
@@ -3,36 +3,37 @@
 # version: 1.0.0
 # authors: Jacob Gillespie
 # url: https://github.com/the-obsidian/discourse-minecraft-avatar
+after_initialize do
+  # override User avatar
+  class User
+    MC_BASE_URI = 'https://api.mojang.com/users/profiles/minecraft'
 
-# override User avatar
-class User
-  MC_BASE_URI = 'https://api.mojang.com/users/profiles/minecraft'
+    def minecraft_uuid
+      uuid = try(:single_sign_on_record).try(:external_id)
 
-  def minecraft_uuid
-    uuid = try(:single_sign_on_record).try(:external_id)
+      return uuid if uuid
 
-    return uuid if uuid
+      uri = "#{MC_BASE_URI}/#{username}?at=#{Time.now.to_i}"
+      response = RestClient.get uri
+      return false if response.code != 200
 
-    uri = "#{MC_BASE_URI}/#{username}?at=#{Time.now.to_i}"
-    response = RestClient.get uri
-    return false if response.code != 200
-
-    begin
-      JSON.parse(response.body)['id']
-    rescue StandardError => _e
-      1 # return a default
+      begin
+        JSON.parse(response.body)['id']
+      rescue StandardError => _e
+        1 # return a default
+      end
     end
-  end
 
-  def minecraft_avatar
-    "https://crafatar.com/avatars/#{minecraft_uuid}?overlay=true&size={size}"
-  end
+    def minecraft_avatar
+      "https://crafatar.com/avatars/#{minecraft_uuid}?overlay=true&size={size}"
+    end
 
-  def avatar_template
-    return minecraft_avatar unless uploaded_avatar_id
-    hostname = RailsMultisite::ConnectionManagement.current_hostname
-    UserAvatar.local_avatar_template(
-      hostname, username.downcase, uploaded_avatar_id
-    )
+    def avatar_template
+      return minecraft_avatar unless uploaded_avatar_id
+      hostname = RailsMultisite::ConnectionManagement.current_hostname
+      UserAvatar.local_avatar_template(
+        hostname, username.downcase, uploaded_avatar_id
+      )
+    end
   end
 end
